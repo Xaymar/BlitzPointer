f979c2ca4bb7683b794b51da5a3e7b9368bad16a
Michael Fabian Dirks <michael.dirks@realitybends.de>
2015-06-06 18:19:10 +0200

    BlitzPointer is now licensed under LGPLv3.0. Read LICENSE and LICENSE.lesser if you don't know what restrictions these give you.
    Signed-off-by: Michael Fabian Dirks <michael.dirks@realitybends.de>

--- BlitzPointer.cpp
+++ BlitzPointer.cpp
@@ -1,5 +1,21 @@
-// Original Code by Noodoby<http://www.blitzforum.de/forum/viewtopic.php?t=31651>
-// Modified Code by Xaymar<http://project-kube.de>
+//	BlitzPointer - Adding Pointers to Blitz.
+//	Copyright (C) 2015 Project Kube (Michael Fabian Dirks)
+//
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU Lesser General Public License as
+//	published by the Free Software Foundation, either version 3 of the 
+//	License, or (at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU Lesser General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Idea take from Code by Noodoby<http://www.blitzforum.de/forum/viewtopic.php?t=31651>
+// New Code by Xaymar<http://project-kube.de>
 
 #include "BlitzPointer.h"
 
